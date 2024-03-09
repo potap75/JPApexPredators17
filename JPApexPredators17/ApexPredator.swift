30ea35b78ce8258955dabbc959da057c3df218e3
potap75 <rummaan@me.com>
2024-03-09 00:50:20 -0500

    v0001

---
+++ JPApexPredators17/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators17
+//
+//  Created by Roman Potapov on 3/8/24.
+//
+
+import Foundation
---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,8 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by Roman Potapov on 3/9/24.
+//
+
+import Foundation
