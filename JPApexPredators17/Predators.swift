c0d0aef8694b4bae35399c28e333bacb515905a2
potap75 <rummaan@me.com>
2024-03-09 01:28:28 -0500

    v0001

--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class Predators {
+    
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding json: \(error)")
+            }
+        }
+    }
+}
