1fb2bcd56fcf5c8418fc3b3b735d5cf47518ab27
potap75 <rummaan@me.com>
2024-03-15 23:16:06 -0400

    v0003

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -10,40 +10,63 @@ import SwiftUI
 struct ContentView: View {
     
     let predators = Predators()
+    @State var searchText = ""
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter { 
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
     
     var body: some View {
-        List(predators.apexPredators) { predator in 
-            
-            HStack {
-                //dinosaur image
-                
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                    .shadow(color: .white,radius: 1)
-                
-                VStack(alignment: .leading) {
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
                     
-                    // Name
-                    Text(predator.name)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    // Type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
                     
+                    
+                    HStack {
+                        //dinosaur image
+                        
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                            .shadow(color: .white,radius: 1)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                            
+                        }
+                    }
                 }
             }
-            }
-        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-            
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
-        
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
     }
 
 #Preview {
--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -16,6 +16,7 @@ class Predators {
     }
     
     func decodeApexPredatorData() {
+        
         if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
